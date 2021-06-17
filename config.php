af00863b1da685bb22d0d34dbfc7a41e544971e8
Arthur Conti <arthurgallotticonti@gmail.com>
2021-06-17 00:18:47 -0300

    all configs variables in uppercase

--- config.php
+++ config.php
@@ -4,18 +4,18 @@
  * Configurações da sua conexão com o banco
  * @param DB_HOST url para a base de dados
  * @param DB_USER usuário de acesso ao banco
- * @param DB_password senha do usuario de acesso ao banco
- * @param DB_database banco padrão para acesso da conexão
+ * @param DB_PASSWORD senha do usuario de acesso ao banco
+ * @param DB_DATABASE banco padrão para acesso da conexão
  * @param DB_PORT porta de acesso para o banco
  */
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_password', '');
-define('DB_database', 'db_empresa');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'db_empresa');
 define('DB_PORT', 3306);
 
-$databaseConn = mysqli_connect(DB_HOST, DB_USER, DB_password, DB_database, DB_PORT);
+$databaseConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
 
 if (!$databaseConn) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
